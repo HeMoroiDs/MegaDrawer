acd1d256d8b073395aed463fe8ba926f2ac10883
pj <hardibopj@yahoo.fr>
2013-12-15 21:49:19 +0100

    initial

---
+++ MegaDrawer/src/MegaDrawer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MegaDrawer.cpp
+// Author      : PJ
+// Version     :
+// Copyright   : CopyLeft
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!! hello world de merde !!!" << endl; // prints !! hello world de merde !!!
+	return 0;
+}
